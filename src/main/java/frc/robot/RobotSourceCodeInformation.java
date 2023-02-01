a94cf88f8f553331cdc64acd20b7b2fd673ad950
Christopher Lankow <clankow@comcast.net>
2023-02-01 17:38:58 -0600

    Add working directory status.

--- src/main/java/frc/robot/RobotSourceCodeInformation.java
+++ src/main/java/frc/robot/RobotSourceCodeInformation.java
@@ -16,9 +16,11 @@ public class RobotSourceCodeInformation {
     private String branchString;
     private String commitString;
     private String dateString;
+    private String workingDirStatusString;
     private StringPublisher branchPub;
     private StringPublisher commitPub;
     private StringPublisher datePub;
+    private StringPublisher workingDirStatusPub;
   
     public RobotSourceCodeInformation() {
       table = NetworkTableInstance.getDefault().getTable("source-code-information");
@@ -29,6 +31,7 @@ public class RobotSourceCodeInformation {
       File branchFile = new File(deployDir, "branch.txt");
       File commitFile = new File(deployDir, "commit.txt");
       File dateFile = new File(deployDir, "date.txt");
+      File workingDirStatusFile = new File(deployDir, "status.txt");
 
       try {
         BufferedReader branchReader = new BufferedReader(new FileReader(branchFile));
@@ -43,17 +46,24 @@ public class RobotSourceCodeInformation {
         dateString = dateReader.readLine();
         dateReader.close();
         
+        BufferedReader workingDirStatusReader = new BufferedReader(new FileReader(workingDirStatusFile));
+        workingDirStatusString = workingDirStatusReader.readLine();
+        workingDirStatusReader.close();
+
         System.out.println(branchString);
         System.out.println(commitString);
         System.out.println(dateString);
+        System.out.println(workingDirStatusString);
 
         branchPub = table.getStringTopic("branch-string").publish();
         commitPub = table.getStringTopic("commit-string").publish();
         datePub = table.getStringTopic("date-string").publish();
+        workingDirStatusPub = table.getStringTopic("working-dir-status-string").publish();
 
         branchPub.set(branchString);
         commitPub.set(commitString);
         datePub.set(dateString);
+        workingDirStatusPub.set(workingDirStatusString);
       }
       catch (Exception e)
       {
