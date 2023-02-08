d9f318e27869d0cd1d4aff52c0da271056215f70
Christopher Lankow <clankow@comcast.net>
2023-02-08 08:29:03 -0600

    Added code from examples.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -83,6 +83,19 @@ public final class Constants {
 
         }
 
+      public static class ShoulderMotorConstants {
+        public static final double kP = 0.1;
+        public static final double kI = 1e-4;
+        public static final double kD = 1;
+        public static final double kIz = 0;
+        public static final double kFF = 0;
+        public static final double kMaxOutput = 1;
+        public static final double kMinOutput = -1;
+        public static final double PICKUP = 0;
+        public static final double PLACE_LOW = 50;
+        public static final double PLACE_HIGH = 100;
+
+      }
     
 
     public static final class OIConstants {
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -2,24 +2,45 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.SparkMaxRelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants.CANChannels;
+import frc.robot.Constants.DriveConstants.ShoulderMotorConstants;
 
 public class ArmSubsystem extends SubsystemBase {
 
     final CANSparkMax m_shoulder;
     final RelativeEncoder m_shoulderEncoder;
+    final SparkMaxPIDController m_shoulderPidController;
+  
     final CANSparkMax m_elevator;
     final RelativeEncoder m_elevatorEncoder;
      
       /** Creates a new Arm Subsystem. */
-      public ArmSubsystem() {
+    public ArmSubsystem() {
         m_shoulder = new CANSparkMax(CANChannels.SHOULDER, MotorType.kBrushed);
+        m_shoulderEncoder = m_shoulder.getEncoder(SparkMaxRelativeEncoder.Type.kQuadrature, 4096);
+        m_shoulderPidController = m_shoulder.getPIDController();
+        m_shoulderPidController.setFeedbackDevice(m_shoulderEncoder);
+        // set PID coefficients
+        m_shoulderPidController.setP(ShoulderMotorConstants.kP);
+        m_shoulderPidController.setI(ShoulderMotorConstants.kI);
+        m_shoulderPidController.setD(ShoulderMotorConstants.kD);
+        m_shoulderPidController.setIZone(ShoulderMotorConstants.kIz);
+        m_shoulderPidController.setFF(ShoulderMotorConstants.kFF);
+        m_shoulderPidController.setOutputRange(ShoulderMotorConstants.kMinOutput, ShoulderMotorConstants.kMaxOutput);
+
+
         m_elevator = new CANSparkMax(CANChannels.ELEVATOR, MotorType.kBrushless);
-        m_shoulderEncoder = m_shoulder.getEncoder();
         m_elevatorEncoder = m_elevator.getEncoder();
-      }  
-    
+    }
+
+    public void setPosition(double position) {
+        m_shoulderPidController.setReference(position, CANSparkMax.ControlType.kPosition);
+    }
+     
 }
